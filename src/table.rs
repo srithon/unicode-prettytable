cb22eb05ca4d3e2a97efc5ad701f1c62fd02ace4
Sridaran Thoniyil <sri7thon@gmail.com>
2021-02-05 16:13:48 -0500

    Define unicode table primitives in Table module

---
+++ src/table.rs
@@ -0,0 +1,14 @@
+// https://www.unicode.org/charts/PDF/U2500.pdf
+const HORIZONTAL: char = '\u{2500}'; // ─
+const VERTICAL: char = '\u{2502}'; // │
+
+const TOP_BRACE: char = '\u{252C}'; // ┬
+const BOTTOM_BRACE: char = '\u{2534}'; // ┴
+const LEFT_BRACE: char = '\u{251c}'; // ├
+const RIGHT_BRACE: char = '\u{2524}'; // ┤
+const MIDDLE_BRACE: char = '\u{253C}'; // ┤
+
+const TOP_LEFT_CORNER: char = '\u{250c}'; // ┌
+const TOP_RIGHT_CORNER: char = '\u{2510}'; // ┐
+const BOTTOM_RIGHT_CORNER: char = '\u{2518}'; // ┘
+const BOTTOM_LEFT_CORNER: char = '\u{2514}'; // └
