638e171740589f584f0f54bb900fc21fa5e45882
Sridaran Thoniyil <sri7thon@gmail.com>
2021-02-07 17:49:23 -0500

    Move functionality into Table; table_to_string into fmt

--- src/table.rs
+++ src/table.rs
@@ -1,5 +1,6 @@
 use crate::util::StringBuffer;
 
+use std::fmt;
 use derive_builder::Builder;
 
 // https://www.unicode.org/charts/PDF/U2500.pdf
@@ -28,14 +29,6 @@ const TOP_RIGHT_CORNER_HEADER: &str = "\u{2555}"; // ╕
 const BOTTOM_RIGHT_CORNER: &str = "\u{2518}"; // ┘
 const BOTTOM_LEFT_CORNER: &str = "\u{2514}"; // └
 
-/// Creates a list of byte vectors corresponding to each string of horizontal separators
-fn generate_horizontal_separators(column_widths: &Vec<usize>, horizontal_char: &str) -> Vec<Vec<u8>> {
-    column_widths
-        .iter()
-        .map(|&length| horizontal_char.repeat(length).into_bytes())
-        .collect::<Vec<_>>()
-}
-
 #[derive(Builder, Clone)]
 pub struct Header {
     /// Whether to use double bar Unicode characters surrounding the header
@@ -57,188 +50,206 @@ where
     rows: &'a Vec<Vec<T>>,
 }
 
-/// Given a 2D input, returns the minimum width of each column in a vector
-fn get_column_widths<'a, T: 'a>(input: &Vec<Vec<T>>, center_headers: bool) -> Vec<usize>
+impl <'a, T> Table<'a, T>
 where
     T: AsRef<str>,
     &'a T: AsRef<str>
 {
-    let header_widths: Vec<usize> = {
-        if let Some(row) = input.get(0) {
-            let header_padding = {
-                if center_headers {
-                    2
-                }
-                else {
-                    0
-                }
-            };
-
-            row.iter().map(|entry| entry.as_ref().len() + header_padding).collect()
-        }
-        else {
-            return Vec::new();
-        }
-    };
-
-    let mut widths = input
-    .iter()
-    .map(|row| row
-        .into_iter()
-        .map(|entry| entry.as_ref().len())
-    )
-    .skip(1)
-    .fold(header_widths.clone(), |mut column_widths: Vec<usize>, row| {
-        for (a, b) in column_widths.iter_mut().zip(row) {
-            // if b is bigger than the current column width, set the new column width to b
-            *a = b.max(*a);
-        }
-
+    /// Creates a list of byte vectors corresponding to each string of horizontal separators
+    fn generate_horizontal_separators(column_widths: &Vec<usize>, horizontal_char: &str) -> Vec<Vec<u8>> {
         column_widths
-    });
-
-    if center_headers {
-        // make sure that header has an even number of spaces on either side
-        for (col_width, header_width) in widths.iter_mut().zip(header_widths.into_iter()) {
-            let num_spaces = *col_width - header_width;
-
-            // let there be a space on either side
-            if num_spaces == 0 {
-                *col_width += 2;
-            }
-            // make it an even number of spaces
-            else if num_spaces % 2 == 1 {
-                *col_width += 1;
-            }
-        }
+            .iter()
+            .map(|&length| horizontal_char.repeat(length).into_bytes())
+            .collect::<Vec<_>>()
     }
 
-    widths
+    /// Given a 2D input, returns the minimum width of each column in a vector
+    fn get_column_widths(&self) -> Vec<usize>
+    where
+        T: AsRef<str>,
+        &'a T: AsRef<str>
+    {
+        // default to false if there are no header settings
+        let center_headers = self.header.as_ref().map(|settings| settings.centered_text).unwrap_or(false);
+
+        let header_widths: Vec<usize> = {
+            if let Some(row) = self.rows.get(0) {
+                let header_padding = {
+                    if center_headers {
+                        2
+                    }
+                    else {
+                        0
+                    }
+                };
+
+                row.iter().map(|entry| entry.as_ref().len() + header_padding).collect()
+            }
+            else {
+                return Vec::new();
+            }
+        };
+
+        let mut widths = self.rows
+            .iter()
+            .map(|row| row
+                .into_iter()
+                .map(|entry| entry.as_ref().len())
+            )
+            .skip(1)
+            .fold(header_widths.clone(), |mut column_widths: Vec<usize>, row| {
+                for (a, b) in column_widths.iter_mut().zip(row) {
+                    // if b is bigger than the current column width, set the new column width to b
+                    *a = b.max(*a);
+                }
+
+                column_widths
+            });
+
+        if center_headers {
+            // make sure that header has an even number of spaces on either side
+            for (col_width, header_width) in widths.iter_mut().zip(header_widths.into_iter()) {
+                let num_spaces = *col_width - header_width;
+
+                // let there be a space on either side
+                if num_spaces == 0 {
+                    *col_width += 2;
+                }
+                // make it an even number of spaces
+                else if num_spaces % 2 == 1 {
+                    *col_width += 1;
+                }
+            }
+        }
+
+        widths
+    }
 }
 
-/// Given a 2D vector of (coercable) str's, returns a string representing the data in a table
-pub fn table_to_string<'a, T: 'a>(input: Vec<Vec<T>>, first_row_header: bool, center_headers: bool) -> String
+impl <'a, T> fmt::Display for Table<'a, T>
 where
     T: AsRef<str>,
     &'a T: AsRef<str>,
 {
-    let column_widths = get_column_widths(&input, center_headers);
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let column_widths = self.get_column_widths();
 
-    let total_width_per_row = {
-        // one separator to the left of each one, as well as one separator on the very right
-        let num_separators_per_row = column_widths.len() + 1;
+        let total_width_per_row = {
+            // one separator to the left of each one, as well as one separator on the very right
+            let num_separators_per_row = column_widths.len() + 1;
 
-        let base_width_per_row: usize = column_widths.iter().sum();
+            let base_width_per_row: usize = column_widths.iter().sum();
 
-        base_width_per_row + num_separators_per_row
-    };
-
-    let string_length = {
-        let total_lines = {
-            let num_rows = input.len();
-
-            // separator above each row PLUS one under the last row
-            let num_separator_lines = num_rows + 1;
-
-            num_separator_lines + num_rows
+            base_width_per_row + num_separators_per_row
         };
 
-        // no newline after the last line
-        let num_newlines = total_lines - 1;
+        let string_length = {
+            let total_lines = {
+                let num_rows = self.rows.len();
 
-        (total_width_per_row * total_lines) + num_newlines
-    };
+                // separator above each row PLUS one under the last row
+                let num_separator_lines = num_rows + 1;
 
-    // fill string with spaces
-    let mut buffer = StringBuffer::with_capacity_fill(string_length, ' ');
+                num_separator_lines + num_rows
+            };
 
-    let standard_horizontal_separators = generate_horizontal_separators(&column_widths, HORIZONTAL);
-    let header_horizontal_separators = generate_horizontal_separators(&column_widths, HORIZONTAL_HEADER);
+            // no newline after the last line
+            let num_newlines = total_lines - 1;
 
-    let create_sep_row = |horizontal_separators: &Vec<Vec<u8>>, left_char, middle_char, right_char, newline| {
-        let mut sep_buffer = {
-            // allocate one extra byte if there is a newline at the end
-            let newline_increment = if newline { 1 } else { 0 };
-            let capacity = total_width_per_row + newline_increment;
-            StringBuffer::with_capacity(capacity)
+            (total_width_per_row * total_lines) + num_newlines
         };
 
-        sep_buffer.push_chars(left_char);
+        // fill string with spaces
+        let mut buffer = StringBuffer::with_capacity_fill(string_length, ' ');
 
-        let (last_sep, seps) = horizontal_separators.split_last().unwrap();
+        let standard_horizontal_separators = Self::generate_horizontal_separators(&column_widths, HORIZONTAL);
+        let header_horizontal_separators = Self::generate_horizontal_separators(&column_widths, HORIZONTAL_HEADER);
 
-        for sep in seps {
-            sep_buffer.push_bytes(sep);
-            sep_buffer.push_chars(middle_char);
-        }
+        let create_sep_row = |horizontal_separators: &Vec<Vec<u8>>, left_char, middle_char, right_char, newline| {
+            let mut sep_buffer = {
+                // allocate one extra byte if there is a newline at the end
+                let newline_increment = if newline { 1 } else { 0 };
+                let capacity = total_width_per_row + newline_increment;
+                StringBuffer::with_capacity(capacity)
+            };
 
-        sep_buffer.push_bytes(&last_sep);
-        sep_buffer.push_chars(right_char);
+            sep_buffer.push_chars(left_char);
 
-        if newline {
-            sep_buffer.push_chars("\n")
-        }
+            let (last_sep, seps) = horizontal_separators.split_last().unwrap();
 
-        sep_buffer.into_buffer()
-    };
+            for sep in seps {
+                sep_buffer.push_bytes(sep);
+                sep_buffer.push_chars(middle_char);
+            }
 
-    let mut input_iterator = input.into_iter().peekable();
+            sep_buffer.push_bytes(&last_sep);
+            sep_buffer.push_chars(right_char);
 
-    macro_rules! push_data_row {
-        ($col_formatter:expr) => {
-            if let Some(row) = input_iterator.next() {
-                buffer.push_chars(VERTICAL);
-                for (col_index, col) in row.into_iter().enumerate() {
-                    let base = col.as_ref();
+            if newline {
+                sep_buffer.push_chars("\n")
+            }
 
-                    buffer.push_chars_fixed_width($col_formatter(base, column_widths[col_index]).as_ref(), column_widths[col_index]);
+            sep_buffer.into_buffer()
+        };
+
+        let mut input_iterator = self.rows.iter().peekable();
+
+        macro_rules! push_data_row {
+            ($col_formatter:expr) => {
+                if let Some(row) = input_iterator.next() {
                     buffer.push_chars(VERTICAL);
-                }
+                    for (col_index, col) in row.into_iter().enumerate() {
+                        let base = col.as_ref();
 
-                buffer.push_chars("\n");
+                        buffer.push_chars_fixed_width($col_formatter(base, column_widths[col_index]).as_ref(), column_widths[col_index]);
+                        buffer.push_chars(VERTICAL);
+                    }
+
+                    buffer.push_chars("\n");
+                }
             }
         }
-    }
 
-    if first_row_header {
-        let header_top = create_sep_row(&header_horizontal_separators, TOP_LEFT_CORNER_HEADER, TOP_BRACE_HEADER, TOP_RIGHT_CORNER_HEADER, true);
-        buffer.push_bytes(&header_top);
+        if first_row_header {
+            let header_top = create_sep_row(&header_horizontal_separators, TOP_LEFT_CORNER_HEADER, TOP_BRACE_HEADER, TOP_RIGHT_CORNER_HEADER, true);
+            buffer.push_bytes(&header_top);
 
-        if center_headers {
-            // align header text in the center of each column
-            push_data_row!(|base_str, width| format!("{:^width$}", base_str, width = width));
+            if center_headers {
+                // align header text in the center of each column
+                push_data_row!(|base_str, width| format!("{:^width$}", base_str, width = width));
+            }
+            else {
+                push_data_row!(|base_str, _| base_str);
+            }
+
+            let header_bottom = create_sep_row(&header_horizontal_separators, LEFT_BRACE_HEADER, MIDDLE_BRACE_HEADER, RIGHT_BRACE_HEADER, true);
+            buffer.push_bytes(&header_bottom);
         }
         else {
+            let header = create_sep_row(&standard_horizontal_separators, TOP_LEFT_CORNER, TOP_BRACE, TOP_RIGHT_CORNER, true);
+            // add header
+            buffer.push_bytes(&header);
+        }
+
+        let standard_separator = create_sep_row(&standard_horizontal_separators, LEFT_BRACE, MIDDLE_BRACE, RIGHT_BRACE, true);
+        let footer = create_sep_row(&standard_horizontal_separators, BOTTOM_LEFT_CORNER, BOTTOM_BRACE, BOTTOM_RIGHT_CORNER, false);
+
+        loop {
             push_data_row!(|base_str, _| base_str);
+
+            // only create a standard separator row if there are more data rows
+            if input_iterator.peek().is_some() {
+                buffer.push_bytes(&standard_separator);
+            }
+            else {
+                // break out if there are no more data rows
+                break;
+            }
         }
 
-        let header_bottom = create_sep_row(&header_horizontal_separators, LEFT_BRACE_HEADER, MIDDLE_BRACE_HEADER, RIGHT_BRACE_HEADER, true);
-        buffer.push_bytes(&header_bottom);
+        // add footer
+        buffer.push_bytes(&footer);
+
+        write!(f, "{}", buffer.to_string())
     }
-    else {
-        let header = create_sep_row(&standard_horizontal_separators, TOP_LEFT_CORNER, TOP_BRACE, TOP_RIGHT_CORNER, true);
-        // add header
-        buffer.push_bytes(&header);
-    }
-
-    let standard_separator = create_sep_row(&standard_horizontal_separators, LEFT_BRACE, MIDDLE_BRACE, RIGHT_BRACE, true);
-    let footer = create_sep_row(&standard_horizontal_separators, BOTTOM_LEFT_CORNER, BOTTOM_BRACE, BOTTOM_RIGHT_CORNER, false);
-
-    loop {
-        push_data_row!(|base_str, _| base_str);
-
-        // only create a standard separator row if there are more data rows
-        if input_iterator.peek().is_some() {
-            buffer.push_bytes(&standard_separator);
-        }
-        else {
-            // break out if there are no more data rows
-            break;
-        }
-    }
-
-    // add footer
-    buffer.push_bytes(&footer);
-
-    buffer.to_string()
 }
